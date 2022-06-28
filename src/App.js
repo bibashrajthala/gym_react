00dc0f4d259fb165d1a473cfbe875345e534f4a4
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-06-28 16:55:09 +0545

    Structure files and folders ,Implement routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import ExerciseDetail from "./pages/ExerciseDetail/ExerciseDetail";
+
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box width="400px" sx={{ width: { xl: "1448px" } }} m="auto">
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
