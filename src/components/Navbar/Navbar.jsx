0deba2c952531067e1e497e4a93a26a587da6539
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-06-28 18:05:51 +0545

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/Logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction="row"
+      spacing={{ sm: 12.2, xs: 4 }}
+      sx={{
+        // gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        px: "20px",
+      }}
+    >
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "48px", height: "48px", margin: "0 20px" }}
+        />
+      </Link>
+      <Stack direction="row" spacing={4} alignItems="flex-end" fontSize="24px">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3a1212",
+            borderBottom: "3px solid #ff2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3a1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Navbar;
