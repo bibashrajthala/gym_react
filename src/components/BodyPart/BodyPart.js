214f74d15b1eaa348a8ff640c05d54c1568e5dab
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-06-30 03:20:42 +0545

    add bodypart/categories cards/components in the horizontal slider

---
+++ src/components/BodyPart/BodyPart.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+
+import Icon from "../../assets/icons/gym.png";
+
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      spacing={4.7}
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        boxShadow: "0 0 25px rgba(0,0,0,0.2)",
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
+    >
+      <img
+        src={Icon}
+        alt="dumbBell"
+        style={{ width: "40px", height: "40px" }}
+      />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3a1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default BodyPart;
