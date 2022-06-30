391bd23716806295ae8c6ff04f9e3a78c54bd744
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-06-30 13:35:12 +0545

    add exerxise card on search

--- src/components/ExerciseCard/ExerciseCard.jsx
+++ src/components/ExerciseCard/ExerciseCard.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack, Typography, Button } from "@mui/material";
 
-const ExerciseCard = () => {
-  return <div>ExerciseCard</div>;
+const ExerciseCard = ({ exercise }) => {
+  console.log(exercise); // only appear when you serach something eg:back
+  return (
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack flexDirection="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            backgroundColor: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capatalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            backgroundColor: "#fcc757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capatalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        ml="21px"
+        color="#000"
+        fontWeight="bold"
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+        fontSize="22px"
+      >
+        {exercise.name}
+      </Typography>
+    </Link>
+  );
 };
 
 export default ExerciseCard;
