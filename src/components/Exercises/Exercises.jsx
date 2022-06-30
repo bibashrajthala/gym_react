c5cc523c8822eab7752b6c34887ae5235a370122
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-06-30 13:35:59 +0545

    add/implement pagination in exercises section

--- src/components/Exercises/Exercises.jsx
+++ src/components/Exercises/Exercises.jsx
@@ -1,27 +1,56 @@
 import React, { useState, useEffect } from "react";
-import { Pagination } from "@mui/material/Pagination";
+import { Pagination } from "@mui/material";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../../utils/fetchData";
 import ExerciseCard from "../ExerciseCard/ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-  console.log(exercises); // only appear when you serach something
+  // console.log(exercises); // only appear when you serach something
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: "1800px", behavior: "smooth" });
+  };
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
-      <Typography variant="h3" mb="46px">
+      <Typography variant="h3" mb="46px" ml="50px">
         Showing Results
       </Typography>
       <Stack
         flexDirection="row"
         spacing={{ lg: 11, xs: 5 }}
         flexWrap="wrap"
-        justifyContent="center"
+        justifyContent={{ xs: "center", sm: "space-between" }}
+        px="50px"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
           // <p>{exercises.name}</p>
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          ></Pagination>
+        )}
       </Stack>
     </Box>
   );
